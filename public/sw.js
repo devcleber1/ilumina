9ac6f56bfe9e264da3a9bffa21cdec6d2c4d64a9
devcleber1 <clebinhoalves2011@gmail.com>
2026-06-04 19:17:35 -0300

    geração do pwa

---
+++ public/sw.js
@@ -0,0 +1,78 @@
+const CACHE_NAME = 'ilumina-pwa-v2'
+const OFFLINE_URL = '/offline.html'
+const ASSETS = [
+  '/',
+  OFFLINE_URL,
+  '/manifest.json',
+  '/apple-touch-icon.png',
+  '/favicon-16x16.png',
+  '/favicon-32x32.png',
+  '/android-chrome-192x192.png',
+  '/android-chrome-512x512.png',
+]
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  )
+})
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then(keys =>
+        Promise.all(keys.map(key => (key !== CACHE_NAME ? caches.delete(key) : Promise.resolve())))
+      )
+      .then(() => self.clients.claim())
+  )
+})
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') {
+    return
+  }
+
+  const requestUrl = new URL(event.request.url)
+  const isSameOrigin = requestUrl.origin === self.location.origin
+
+  if (event.request.mode === 'navigate' && isSameOrigin) {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          return response
+        })
+        .catch(() => caches.match(OFFLINE_URL))
+    )
+    return
+  }
+
+  if (!isSameOrigin) {
+    return
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse
+      }
+      return fetch(event.request)
+        .then(networkResponse => {
+          if (
+            !networkResponse ||
+            networkResponse.status !== 200 ||
+            networkResponse.type !== 'basic'
+          ) {
+            return networkResponse
+          }
+          const responseClone = networkResponse.clone()
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, responseClone))
+          return networkResponse
+        })
+        .catch(() => caches.match(event.request))
+    })
+  )
+})
