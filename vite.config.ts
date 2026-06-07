b56609df81ba5f18f86000bd7f220da53fe3bf96
devcleber1 <clebinhoalves2011@gmail.com>
2026-06-07 20:49:03 -0300

    Testando a api no render

--- vite.config.ts
+++ vite.config.ts
@@ -11,11 +11,21 @@ export default defineConfig({
     },
   },
   server: {
+    host: '127.0.0.1',
+    hmr: {
+      host: '127.0.0.1',
+      protocol: 'ws',
+    },
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://ilumina-server.onrender.com',
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, ''),
+        configure: (proxy) => {
+          proxy.on('proxyReq', (proxyReq) => {
+            proxyReq.removeHeader('origin');
+          });
+        },
       },
     },
   },
